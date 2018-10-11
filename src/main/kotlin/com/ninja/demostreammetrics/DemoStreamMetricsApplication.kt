c1c9361f192a9d9bfb9308ae5c4ca97a4874e48a
takaaki.iida <takiida0605@gmail.com>
2018-10-11 16:42:56 +0900

    first commit

---
+++ src/main/kotlin/com/ninja/demostreammetrics/DemoStreamMetricsApplication.kt
@@ -0,0 +1,11 @@
+package com.ninja.demostreammetrics
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoStreamMetricsApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoStreamMetricsApplication>(*args)
+}
---
+++ src/test/kotlin/com/ninja/demostreammetrics/DemoStreamMetricsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.ninja.demostreammetrics
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DemoStreamMetricsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
